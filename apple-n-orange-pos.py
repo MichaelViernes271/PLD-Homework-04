277edfe03d996d6ba33f33c5b9239956f6980e70
Michael Viernes <92641515+MichaelViernes271@users.noreply.github.com>
2021-11-12 23:32:23 +0800

    Major renaming of variables, and experiments.
    commit 7dcdb77935a23be8753be7a9ceed8dc260e7fbd8 (HEAD)
    Author: MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
    Date:   Fri Nov 12 23:05:17 2021 +0800
    
         Changes to be committed:
                modified:   apple-n-orange-pos.py
         Run and tested files. Changes were:
    
                1. Experimented on str.upper();
                2. Rearranged main func;
                3. Renames variables with 'm' & 'user' conventions;
                4. Tests 'if statements'; &
                5. Tested.

--- apple-n-orange-pos.py
+++ apple-n-orange-pos.py
@@ -1,42 +1,37 @@
 #apple and orange pos
 """
-Programming Logic and Design: Homework 03
+Topic: Programming Logic and Design
 Author: Viernes, Michael
 Submitted to: Mr. Madrigalejos
 """
 
-print("-------------------------------")
-print("\tApple and orange")
-print("-------------------------------")
-
-def printTotal(sum):
-    "Prints total."
+def printTotal(sum): # Prints total.
     print(f"The total amount of fruits is {sum}.")
+    # end printTotal()
 
-def calcFruits(apples, orange):
-    "Calculates sum of fruits."
+def calcFruits(apples, orange): # Calculates sum of fruits.
     sum = (apples * 20) + (orange * 25)
-    
     print("$20 * Apples = " + str(apples * 20))
     print("$25 * Orange = " + str(orange * 25))
     return sum
+    # end calcFruits()
 
-def payFruits():
-    "Customer pays."
+def payFruits(): # Customer pays.
+    print("-------------------------------")
+    print("\tApple and orange")
+    print("-------------------------------")
     
     customer = input("Hello, Mr/Ms: ")
     print(f"Hello {customer}\n")
     
-    
     try:
         apples = int(input(f"How many apples will you buy: "))
         orange = int(input(f"How many orange will you buy: "))
-        
     except Exception:
         print("Please input a number. \n")
             
     sum = calcFruits(apples, orange)
     printTotal(sum)
+    # end payFruits()
 
-# Call payFruits
-payFruits()
+payFruits()# Call payFruits
--- name-intro.py
+++ name-intro.py
@@ -26,32 +26,38 @@ def getAddress():
 
 def getInfo(): # gets name, age, address
     # declare info local variables
-    name, age, address = None
-    while name and age and address == None:
-        name = input("Your name: ")
-        age = input("Your age: ")
-        address = input("Your address: ")
+    name, age, address = "", "", ""
+    name = input("Your name: ")
+    age = input("Your age: ")
+    address = input("Your address: ")
     return name, age, address # end getInfo()
 
-def printInfo(userInfo):
-    print(f"Hi, my name is {userInfo[0]}. I am {userInfo[1]} years old and I live in {userInfo[2]}.")
+def printInfo(user_name, user_age, user_address):
+    print(f"Hi, my name is {user_name}. I am {user_age} years old and I live in {user_address}.")
 
-# define main program
-def main():
-    # Creating array.
-    info = []
-    try:
-        info.append(getName())
-        info.append(getAge())
-        info.append(getAddress())
-    except Error:
-        print("Please put a proper input.")
-    printInfo(info)
+def main(): # define main program
+    """
+    Creating array. # THE FOLLOWING CODE WILL NOT BE USED!
+    #info = []
+        # THE FOLLOWING CODE WILL NOT BE USED!
+        #info.append(getName())
+        #info.append(getAge())
+        #info.append(getAddress())
+    """
+    
+    m_name ,m_age ,m_address = getInfo()
+    printInfo(m_name, m_age, m_address)
 
 while True:
     quit = "None"
     main() # call main program
     quit = input("Press Q to quit: ")
-    if quit == "q".upper()
+    if str(quit).upper() == "Q": # Experimented on str's upper func.
+    
+        # Experimented on multi line prints.
+        
+        print(
+        """Thank you for your participation."""
+        )
         break
 
